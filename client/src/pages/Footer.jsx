aeb42e02039cb523fc1df1bdeb5d83f7c9de330a
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2024-02-01 18:57:48 +0530

    error handling and user experience done

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -2,13 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto text-center">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} English Quest. All rights reserved.
-        </p>
-      </div>
-    </footer>
+    <>
+      <br />
+      <br />
+      <footer className="bg-gray-800 text-white py-4 fixed bottom-0 w-full">
+        <div className="container mx-auto text-center">
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} English Quest. All rights
+            reserved.
+          </p>
+        </div>
+      </footer>
+    </>
   );
 };
 
