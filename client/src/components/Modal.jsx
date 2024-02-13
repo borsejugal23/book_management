df0bb6728c667fae87e73ff660a43f30d5261204
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2024-02-13 14:47:24 +0530

    modal update done

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ isOpen, closeModal, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
-      <div className="relative top-40 mx-auto p-5 border w-11/12 md:w-2/3 lg:w-1/2 shadow-lg rounded-md bg-white">
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
+      <div className="relative top-1/2 transform -translate-y-1/2 mx-auto p-5 border w-11/12 md:w-2/3 lg:w-1/2 shadow-lg rounded-md bg-white z-50">
         <div className="mt-0">
           <div className="mx-auto flex justify-between">
-            <button onClick={closeModal} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
-              <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+            
+            <button
+              onClick={closeModal}
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+            >
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
               </svg>
             </button>
           </div>
-          <div className="mt-2 px-7 py-3">
-            {children}
-          </div>
-          
+          <div className="mt-2 px-7 py-3">{children}</div>
         </div>
       </div>
     </div>
